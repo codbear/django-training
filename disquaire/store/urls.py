1602f6961eefae00f6fae28709e6013a0433ba54
codbear <douglas.meunier@openclassrooms.com>
2021-11-07 19:21:03 +0100

    feat(store): create listing, single and search views

--- disquaire/store/urls.py
+++ disquaire/store/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'store'
 urlpatterns = [
-    path('', views.listing),
-    path('albums/<int:album_id>/', views.single),
-    path('search/', views.search),
+    path('', views.listing, name='listing'),
+    path('albums/<int:album_id>/', views.single, name='single'),
+    path('search/', views.search, name='search'),
 ]
--- disquaire/store/views.py
+++ disquaire/store/views.py
@@ -1,38 +1,38 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Artists, Albums
 
 
-def format_album_list(albums):
-    formatted_albums = [f"<li>{album.title}</li>" for album in albums]
-    return """<ul>{}</ul>""".format('\n'.join(formatted_albums))
-
-
 def index(request):
-    albums = Albums.objects.filter(is_available=True).order_by("-created_at")[:12]
-    template = loader.get_template("store/index.html")
-    context = {"albums": albums}
+    albums = Albums.objects.filter(is_available=True).order_by('-created_at')[:12]
+    context = {'albums': albums}
 
-    return HttpResponse(template.render(context, request=request))
+    return render(request, 'store/index.html', context)
 
 
 def listing(request):
     albums = Albums.objects.filter(is_available=True)
-    message = format_album_list(albums)
+    context = {'albums': albums}
 
-    return HttpResponse(message)
+    return render(request, 'store/listing.html', context)
 
 
 def single(request, album_id):
     album = Albums.objects.get(pk=album_id)
-    artists = " ".join([artist.name for artist in album.artists.all()])
-    message = f"Le nom de l'album est {album.title}. Il a été écrit par {artists}"
-    return HttpResponse(message)
+    artists = [artist.name for artist in album.artists.all()]
+    artists_name = " ".join(artists)
+    context = {
+        'album_title': album.title,
+        'artists_name': artists_name,
+        'album_id': album.id,
+        'thumbnail': album.picture,
+    }
+
+    return render(request, 'store/single.html', context)
 
 
 def search(request):
-    query = request.GET.get("query")
+    query = request.GET.get('query')
     if not query:
         albums = Albums.objects.all()
     else:
@@ -41,15 +41,10 @@ def search(request):
     if not albums.exists():
         albums = Albums.objects.filter(artists__name__icontains=query)
 
-    if not albums.exists():
-        message = "Misère de misère, nous n'avons trouvé aucun résultat !"
-    else:
-        albums = [f"<li>{album.title}</li>" for album in albums]
-        message = """
-            Nous avons trouvé les albums correspondant à votre requête ! Les voici :
-            <ul>
-                {}
-            </ul>
-        """.format("</li><li>".join(albums))
+    title = "Résultats pour la requête %s" % query
+    context = {
+        'albums': albums,
+        'title': title
+    }
 
-    return HttpResponse(message)
+    return render(request, 'store/search.html', context)
