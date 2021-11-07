0d36fe26edf13c5b50e1a1093340499a1e264afc
codbear <douglas.meunier@openclassrooms.com>
2021-11-07 14:01:31 +0100

    feat(store): create Contacts, Albums & Bookings models

--- disquaire/store/models.py
+++ disquaire/store/models.py
@@ -1,5 +1,26 @@
 from django.db import models
 
 
-class Artits(models.Model):
+class Artists(models.Model):
     name = models.CharField(max_length=200, unique=True)
+
+
+class Contacts(models.Model):
+    email = models.EmailField(max_length=100)
+    name = models.CharField(max_length=200)
+
+
+class Albums(models.Model):
+    reference = models.CharField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_available = models.BooleanField(default=True)
+    title = models.CharField(max_length=200)
+    picture = models.URLField()
+    artists = models.ManyToManyField(Artists, related_name='albums', blank=True)
+
+
+class Bookings(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    has_been_contacted = models.BooleanField(default=False)
+    album = models.OneToOneField(Albums)
+    contact = models.ForeignKey(Contacts, on_delete=models.CASCADE)
