0c8033db5e54ff8845539065d6f9744aaeebe5a6
codbear <douglas.meunier@openclassrooms.com>
2021-11-07 14:38:04 +0100

    feat(store): add dunder str to models

--- disquaire/store/models.py
+++ disquaire/store/models.py
@@ -4,10 +4,16 @@ from django.db import models
 class Artists(models.Model):
     name = models.CharField(max_length=200, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Contacts(models.Model):
     email = models.EmailField(max_length=100)
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 
 class Albums(models.Model):
@@ -18,9 +24,15 @@ class Albums(models.Model):
     picture = models.URLField()
     artists = models.ManyToManyField(Artists, related_name='albums', blank=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Bookings(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     has_been_contacted = models.BooleanField(default=False)
     album = models.OneToOneField(Albums, on_delete=models.CASCADE)
     contact = models.ForeignKey(Contacts, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.contact.name
