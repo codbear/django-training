4bbc8d8a9b8a7da45774c78f65d218ac7cb87ef7
codbear <douglas.meunier@openclassrooms.com>
2021-11-08 01:00:07 +0100

    feat(admin): add other models to admin

--- disquaire/store/admin.py
+++ disquaire/store/admin.py
@@ -1,8 +1,37 @@
 from django.contrib import admin
 
-from .models import Bookings
+from .models import Bookings, Contacts, Albums, Artists
+
+
+class BookingsInline(admin.TabularInline):
+    model = Bookings
+    fieldsets = [(None, {'fields': ['album', 'has_been_contacted']})]
+    extra = 0
+    verbose_name = 'Réservation'
+    verbose_name_plural = 'Réservations'
+
+
+class AlbumArtistsInline(admin.TabularInline):
+    model = Albums.artists.through
+    extra = 1
+    verbose_name = 'Album'
+    verbose_name_plural = 'Albums'
 
 
 @admin.register(Bookings)
 class BookingsAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ['created_at', 'has_been_contacted']
+
+
+@admin.register(Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+    inlines = [BookingsInline, ]
+
+
+@admin.register(Artists)
+class ArtistsAdmin(admin.ModelAdmin):
+    inlines = [AlbumArtistsInline, ]
+
+@admin.register(Albums)
+class AlbumsAdmin(admin.ModelAdmin):
+    search_fields = ['reference', 'title']
--- disquaire/store/models.py
+++ disquaire/store/models.py
@@ -2,37 +2,49 @@ from django.db import models
 
 
 class Artists(models.Model):
-    name = models.CharField(max_length=200, unique=True)
+    name = models.CharField('nom', max_length=200, unique=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'artiste'
 
 
 class Contacts(models.Model):
-    email = models.EmailField(max_length=100)
-    name = models.CharField(max_length=200)
+    email = models.EmailField('email', max_length=100)
+    name = models.CharField('nom', max_length=200)
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'prospect'
+
 
 class Albums(models.Model):
-    reference = models.CharField(max_length=200, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    is_available = models.BooleanField(default=True)
-    title = models.CharField(max_length=200)
-    picture = models.URLField()
+    reference = models.CharField('référence', max_length=200, null=True)
+    created_at = models.DateTimeField('date de création', auto_now_add=True)
+    is_available = models.BooleanField('disponible', default=True)
+    title = models.CharField('titre', max_length=200)
+    picture = models.URLField('pochette')
     artists = models.ManyToManyField(Artists, related_name='albums', blank=True)
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = 'album'
+
 
 class Bookings(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    has_been_contacted = models.BooleanField(default=False)
+    created_at = models.DateTimeField('date de création', auto_now_add=True)
+    has_been_contacted = models.BooleanField('réservation traitée', default=False)
     album = models.OneToOneField(Albums, on_delete=models.CASCADE)
     contact = models.ForeignKey(Contacts, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.contact.name
+
+    class Meta:
+        verbose_name = 'réservation'
